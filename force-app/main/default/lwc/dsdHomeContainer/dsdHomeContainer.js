c5be36e14417cbd748b9dc33b0f0d25632f4eff8
Bob Teplitsky <bob@fullcircleinsights.com>
2019-06-05 21:17:08 -0700

    Migrating from custom metadata type to custom setting
    Discovered that DML operations aren’t supported in Apex for CMT. https://success.salesforce.com/ideaView?id=08730000000l4TkAAI

--- force-app/main/default/lwc/dsdHomeContainer/dsdHomeContainer.js
+++ force-app/main/default/lwc/dsdHomeContainer/dsdHomeContainer.js
@@ -1,3 +1,10 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+//import getAccountSkewSettings from '@salesforce/apex/DSD_Settings.getAccountSkewSettings'
 
-export default class DsdHomeContainer extends LightningElement {}
+export default class DsdHomeContainer extends LightningElement {
+	// @wire(getAccountSkewSettings) accountSkewSettings;
+
+	// get skewType() {
+	// 	return this.accountSkewSettings.data.MNDSD__DSD_Skew_Type__c;
+	// }
+}
